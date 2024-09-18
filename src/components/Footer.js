3dda2c28a4c673e08aeff298e733017038f47e13
pxigelewis <paige@prepr.org>
2024-09-18 16:15:42 -0400

    Business card project using React

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Footer() {
+    return(
+        <div>
+            <p className="footer-text">2024 Paige Lewis</p>
+        </div>
+    )
+}
+
+export default Footer
---
+++ src/components/Interests.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+function Interests() {
+    return(
+        <div className="interests-section">
+            <h3 className='interests-header'>Interests</h3>
+            <p className="interests-text">Classic and contemporary literature, recipe development and test, langauge learning (I'm currently learning Russian and German!), and Making
+                a perfect coffee every morning.
+            </p>
+        </div>
+    )
+}
+
+export default Interests
