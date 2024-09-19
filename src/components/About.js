e6b4b366044513f08ba534edde4d2448a0a97619
Paige Lewis <51000738+pxigelewis@users.noreply.github.com>
2024-09-19 16:59:39 -0400

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,7 @@ function About() {
         <div className="about-section">
             <h3 className='about-header'>About</h3>
             <p className='about-text'>Hi! I'm a full stack developer with an interest in all things data. I love creating websites that can take large and complext datasets and turn
-                them into something useable. I am love to learn and challenge myself, and I am always thinking up new projects to work on!
+                them into something useable. I love to learn and challenge myself, and I am always thinking up new projects to work on!
             </p>
         </div>
     )
