567b97d5a8226f2dc5385c61dc6b6d1aeb5eb743
Paige Lewis <51000738+pxigelewis@users.noreply.github.com>
2024-09-19 17:00:02 -0400

    Update Interests.js

--- src/components/Interests.js
+++ src/components/Interests.js
@@ -4,7 +4,7 @@ function Interests() {
     return(
         <div className="interests-section">
             <h3 className='interests-header'>Interests</h3>
-            <p className="interests-text">Classic and contemporary literature, recipe development and test, langauge learning (I'm currently learning Russian and German!), and Making
+            <p className="interests-text">Classic and contemporary literature, recipe development and testing, langauge learning (I'm currently learning Russian and German!), and making
                 a perfect coffee every morning.
             </p>
         </div>
